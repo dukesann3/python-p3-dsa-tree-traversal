c93839ece0fa62b75e831f2f9b437e27472d001c
Duke <dukemorise33@gmail.com>
2023-12-16 23:13:59 -0800

    completed lesson

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,26 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+
+    node_to_visit = [self.root]
+    result = []
+
+    while len(node_to_visit) > 0:
+      node = node_to_visit.pop(0)
+      if node['id'] == id:
+        result.append(node)
+        return result[0]
+      node_to_visit = node['children'] + node_to_visit
+    
+    return None
+      
+
+
+
+
+
+
+
+    
+      
+
